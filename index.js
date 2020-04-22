0f85038eba5534ad66b39c1d8652f2ba6f4e3ca3
Dev Ganesh <devganesh91@gmail.com>
2020-04-22 14:33:55 +0530

    Express example

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const dishes = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
+
+const hostname = "localhost";
+const port = 9000;
+
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.use("/dishes", dishes);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
